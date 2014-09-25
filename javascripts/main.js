ec5e1184a83236eb7abd3e698759ea0832807988
Praveen Angyan <angyan@usc.edu>
2014-09-25 09:25:26 -0700

    Adding in fancybox

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,2 +1,19 @@
 $(document).ready(function() {
+  $('.expandable-list h3').click(function() {
+    $(this).next('.answer').slideToggle(250);
+	  $(this).toggleClass('open');
+	});
+
+	$('.fancybox-media').attr('rel', 'media-gallery').fancybox({
+		openEffect : 'none',
+		closeEffect : 'none',
+		prevEffect : 'none',
+		nextEffect : 'none',
+
+		arrows : false,
+		helpers : {
+			media : {},
+			buttons : {}
+		}
+	});
 });
